498c5ac99dc9b0afb7ea5138296d1a3e28c6877d
idtzshivamrajput <mailyourcontentata@gmail.com>
2022-11-19 22:48:20 +0530

    hemlo

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+  })
+
+  app.get('/about', (req, res) => {
+    res.send('About Page')
+  })
+
+  app.get('/contact', (req, res) => {
+    res.send('Contact Page')
+  })
+
+  app.get('/contact1234', (req, res) => {
+    res.send('ye 123 hai bhoi')
+  })
+
+// Server Create
+
+app.listen(port, () => {
+    console.log(`Server is running localhost : ${port}`)
+
+  })
